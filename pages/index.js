16cb532b795bf64044035281644e53f821eab2e7
zhangsaili <cyrilvssll34@gmail.com>
2020-08-16 00:21:33 +0200

    first comit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home () { 
+  return (
+    <>
+      <Head>
+        <title></title>
+      </Head>
+      <header>
+        My Header
+      </header>
+      <main>
+        My body
+      </main>
+    </>
+  );
+
+}
