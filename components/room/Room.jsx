f7b56fd2de733949a49bdf8d2ec51cc982e08c27
zhangsaili <cyrilvssll34@gmail.com>
2020-08-17 14:17:52 +0200

    work on mediaquery on room component and global

--- components/room/Room.jsx
+++ components/room/Room.jsx
@@ -7,6 +7,11 @@ export default function Room() {
         <div className={styles.image1}>
           <img src="/images/Shoot1-433-15.jpg" alt="" />
         </div>
+        <div className={styles.text}>
+          <span>09:30</span>
+          <h1>UN DOUX RÉVEIL</h1>
+          <p>Quelques mots pour décrire le réveil</p>
+        </div>
       </div>
       <div className={styles.container2}>
         <div className={styles.image2}>
