1c2f81212a52f6f6f5208b64fb43ff73cdfe322b
zhangsaili <cyrilvssll34@gmail.com>
2020-08-17 10:37:43 +0200

    {work on room component css }

--- components/room/Room.jsx
+++ components/room/Room.jsx
@@ -3,21 +3,13 @@ import styles from "./room.module.scss";
 export default function Room() {
   return (
     <article className={`container ${styles.article1}`}>
-      <div className={`row  ${styles.room}`}>
-        <div className="col-8">
+      <div className={styles.container1}>
+        <div className={styles.image1}>
           <img src="/images/Shoot1-433-15.jpg" alt="" />
         </div>
-        <div className="col-4 d-flex flex-column align-items-center">
-          <span className="clock">9:23</span>
-          <h2>UN DOUX RÉVEIL</h2>
-          <p className="text-center">
-            LAISSER LA NATURE VOUS RÉVEILLER TOUT EN DOUCEUR
-          </p>
-        </div>
       </div>
-      <div className={`row ${styles.layer}`}>
-        <div className="col-6"></div>
-        <div className={`col ${styles.patron}`}>
+      <div className={styles.container2}>
+        <div className={styles.image2}>
           <img src="/images/suite6-354-15.jpg" alt="" />
         </div>
       </div>
