143f6c4b6161f7052fc0745f761c64c3f3aacd5f
zhangsaili <cyrilvssll34@gmail.com>
2020-08-20 23:08:29 +0200

    Worked on card component

---
+++ components/cards/Card.jsx
@@ -0,0 +1,45 @@
+import styles from "./card.module.scss";
+
+function Card({data}) {
+  return (
+    <div className={`h-100 ${styles.cardContent}`}>
+      <div className={`container ${styles.image}`}>
+        <div className="row ">
+          <div className="col">
+            <img src={`/images/${data.image}`} />
+          </div>
+        </div>
+      </div>
+      <div className={`container ${styles.offer}`}>
+        <div className="row">
+          <div className={`col ${styles.fixedContent}`}>
+            <h2>{data.title}</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className={`col ${styles.fixedContent}`}>
+            <p>{data.description}</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <span>490 EUR</span>
+          </div>
+          <div className="col text-right">
+            <p>{data.persons} personne(s)</p>
+            <p>{data.nights} nuitée(s)</p>
+          </div>
+        </div>
+        <div className="row justify-centent center">
+          <div className="col text-center">
+            <button className={styles.button} type="button">
+              EN SAVOIR PLUS
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,73 @@ import Nav from "../components/navigation/Navigation";
 import Header from "./../components/header/Header";
 import Article1 from "./../components/articles/Article1";
 import Article2 from "./../components/articles/Article2";
+import Card from "./../components/cards/Card";
 import AOS from "aos";
-import { useEffect } from "react";
-
-
-
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [cards, setCards] = useState([
+    {
+      image: "offre1.jpg",
+      title: "NOËL À LA GREE DES LANDES",
+      description: "Le Bien-Être s'invite à Noël",
+      price: "490",
+      persons: "2",
+      nights: "1",
+      type: "stay",
+    },
+    {
+      image: "offre2.jpg",
+      title: "RÉVEILLON DE LA SAINT-SYLVESTRE",
+      description:
+        "Un séjour d'exception pour la fin d'année à partager avec vos amis ou votre famille. Réservation jusqu'au 20/12/2017",
+      price: "1042",
+      persons: "2",
+      nights: "1",
+      type: "stay",
+    },
+    {
+      image: "offre3.jpg",
+      title: "COFFRET CADEAUX",
+      description: [
+        "séjour",
+        "soin et bien être",
+        "Gastronomie",
+        "Cabane d'exception",
+        "Chèques Cadeaux",
+      ],
+      price: "48",
+      persons: "2",
+      nights: "0",
+      type: "present",
+    },
+  ]);
+
   useEffect(() => {
-    AOS.init({duration:2000});
+    AOS.init({ duration: 2000 });
   });
 
   return (
     <>
-      <Nav/>
+      <Nav />
       <main>
-        <Header/>
+        <Header />
         <section>
-          <Article1/>
-          <Article2/>
+          <Article1 />
+          <Article2 />
+        </section>
+        <section className="container">
+          <div className="row">
+            {cards.map((card) => {
+              return (
+                <div className="col">
+                  <Card data={card} />
+                </div>
+              );
+            })}
+          </div>
         </section>
       </main>
-    </>   
+    </>
   );
 }
